67ba3d072d86dfbf9c344561b2d00e352bf82f6e
Vet86 <vitalyev_aleksey@mail.ru>
2020-03-14 23:56:45 +0300

    en: Add telegram bot. ru: Добавлен телеграм бот.

---
+++ CheckClinicBot/ContainerHolder.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace CheckClinic.Bot
+{
+    public static class ContainerHolder
+    {
+        public static IContainer Container { get; }
+        static ContainerHolder()
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<CheckClinicBotModule>();
+            Container = containerBuilder.Build();
+        }
+    }
+}
---
+++ CheckClinicBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CheckClinic.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var telegramProcessor = new TelegramProcessor();
+            //var me = botClient.GetMeAsync().Result;
+            //Console.WriteLine(me.Username);
+            //var markup = new ReplyKeyboardMarkup(new[]
+            //{
+            //        new KeyboardButton("Privet"),
+            //        new KeyboardButton("Hello"),
+            //        new KeyboardButton("Zdarova"),
+            //});
+            //markup.OneTimeKeyboard = true;
+            //botClient.SendTextMessageAsync(chatId, "Hello", replyMarkup: markup);
+            Console.ReadKey();
+        }
+
+        private static void BotClient_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
+        {
+        }
+
+        private static void onMessage(object sender, MessageEventArgs e)
+        {
+        }
+
+        static void HandleMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            
+        }
+    }
+}
